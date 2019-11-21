c4acbcf1fec0bf2458ec9e70c47267632bf9dc61
Mohamed A.bakr <36836206+Shebl27@users.noreply.github.com>
2019-11-21 17:38:53 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,45 @@
+﻿<html>
+    <head>
+        <title>Filter Company</title>
+        <meta charset="utf_8">
+    </head>
+    <body bgcolor=" white green">
+        
+     <i><h1><font size="7" color="#000000"><center><p>Filter Company</p></center></font></h1></i>
+     <marquee bigcolor="white" height,="20" behavior="alternate"><font color="#ffffff" size="5">welcome to our web site </font></marquee><br><br>
+    <br> <hr size="5" color="red">
+        <br>
+<center>
+    <center>
+
+   
+<a href="revenu"><button>Revenu</button></a>
+<a href="expenses"><button>Expenses</button></a>
+<a href="client"><button>Client</button></a>
+<a href="maintance"><button>Maintance</button></a>
+<a href="Categories"><button>Categories</button></a>  
+<a href="Premiums"><button>Premiums</button></a>
+<a href="employee"><button>Employee</button></a>
+
+</center>
+
+  </center>
+        
+        
+    </body>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+</html>
